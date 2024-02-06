51f340a0387a7a5513615aade260b799faac6671
RafaelMagnusson1 <144372295+RafaelMagnusson1@users.noreply.github.com>
2024-02-06 16:58:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,11 @@ from flask import Flask, request, jsonify
 import pickle
 import numpy as np
 
-rf_reg = pickle.load(open(". /modelo_ratio_version1.sav","rb"))
+BASE_DIR = Path(_file_).resolve(strict=True).parent
+
+with open(f"{BASE_DIR}/modelo_ratio_version1.sav", "rb") as f:
+    rf_reg = pickle.load(f)
+        
 colunas = ["sulfato","den_5700_val","diluicao","den_5700_sp"]
 
 
