745efeae9a1a57e1e1a73a6996abd8d9441b5039
RafaelMagnusson1 <144372295+RafaelMagnusson1@users.noreply.github.com>
2024-02-06 16:29:19 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+import pickle
+import numpy as np
+
+rf_reg = pickle.load(open("Model_ratio\modelo_ratio_version1.sav","rb"))
+colunas = ["sulfato","den_5700_val","diluicao","den_5700_sp"]
+
+
+app = Flask(__name__)
+
+@app.route('/ratio/', methods = ["POST"])
+
+def ratio():
+        
+    dados=request.get_json()
+    dados_input = np.array([dados[col] for col in colunas]).reshape(1, -1)
+    ratio = rf_reg.predict(dados_input)
+    return jsonify(ratio = ratio[0])
+
+app.run(debug = True)
